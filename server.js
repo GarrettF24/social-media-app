dc1e0f10f8216588b6ba4de2c54435e67c9628d1
GarrettF <grfoster24@gmail.com>
2022-02-11 10:33:33 -0500

    ran into problems with import and json, es6 and node doesnt support it apparently, going to try some solutions

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-import express from 'express';
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 // looks for enviroment variable called PORT to use when deployed to heroku
 const PORT = process.env.PORT || 5000;
+
+//Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API RUNNING'));
 
