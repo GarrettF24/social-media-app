e16a29ed123d32d389f6f1ad3819894d0bd69c55
GarrettF <grfoster24@gmail.com>
2022-02-25 16:02:56 -0500

    routes and components for sign in and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { Navbar } from './components/Layout/Navbar';
 import { Landing } from './components/Layout/Landing';
-
+import { Login } from './components/Auth/Login';
+import { Signup } from './components/Auth/Signup';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Landing />} />
+        <Route exact path='signup' element={<Signup />} />
+        <Route exact path='login' element={<Login />} />
+      </Routes>
+    </Fragment>
+  </Router>
 );
 
 export default App;
