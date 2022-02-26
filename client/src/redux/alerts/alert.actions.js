01e1af629b10f0a2dcdb71386978408af1a3c1b9
GarrettF <grfoster24@gmail.com>
2022-02-26 12:19:46 -0500

    setting and removing alerts up and running

--- client/src/redux/alerts/alert.actions.js
+++ client/src/redux/alerts/alert.actions.js
@@ -1,10 +1,17 @@
 import { v4 as uuid } from 'uuid';
 import AlertActionTypes from './alert.types';
 
-export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = uuid();
-  dispatch({
-    type: AlertActionTypes.SET_ALERT,
-    payload: { msg, alertType, id },
-  });
-};
+export const setAlert =
+  (msg, alertType, timeout = 4000) =>
+  (dispatch) => {
+    const id = uuid();
+    dispatch({
+      type: AlertActionTypes.SET_ALERT,
+      payload: { msg, alertType, id },
+    });
+
+    setTimeout(
+      () => dispatch({ type: AlertActionTypes.REMOVE_ALERT, payload: id }),
+      timeout
+    );
+  };
